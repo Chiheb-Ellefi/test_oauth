46f75f6e8c3bed66b9c884042003ab71c22ddbd6
Your Name <you@example.com>
2024-03-03 13:49:28 +0100

    first_commit

---
+++ router.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const fetch = require("node-fetch");
+
+const YOUR_CLIENT_ID = "2571c776-fa74-49fa-a092-0ff8474761ba";
+
+const YOUR_CLIENT_SECRET = "74k8Q~ozyjzU.OQZi17PGxGKNZ8.lUq6IcUCibQu";
+
+async function signUp(code, res) {
+  const url = `https://login.microsoftonline.com/common/oauth2/v2.0/token?client_id=2571c776-fa74-49fa-a092-0ff8474761ba&scope=https%3A%2F%2Fgraph.microsoft.com%2Fmail.read&code=${code}&redirect_uri=https%3A%2F%2soft-singers-taste.loca.lt&grant_type=authorization_code&client_secret=74k8Q~ozyjzU.OQZi17PGxGKNZ8.lUq6IcUCibQu`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+  console.log("data : ", data);
+
+  // get the id token from the response
+  /* const { id_token } = data;
+
+  // verify the id token
+  const verifyResponse = await fetch(
+    `https://oauth2.googleapis.com/tokeninfo?id_token=${id_token}`
+  );
+
+  const verifyData = await verifyResponse.json();
+  console.log("verifyData : ", verifyData);
+
+  // get the user data from the verify data
+  const { name, email, picture } = verifyData;
+ */
+  // This res.send is the key to redirecting back to our expo go app.
+  // ex: you have to enter your IP adress that is running your expo go application.
+  res.json({ data });
+}
+
+router.get("/", async (req, res) => {
+  console.log("req.query : ", req.query);
+  res.json("hi");
+  // use the code to get the access token
+
+  const { code } = req.query;
+
+  if (!code) {
+    return res.status(400).json({
+      error: "invalid code",
+    });
+  }
+
+  signUp(code, res);
+});
+
+module.exports = router;
